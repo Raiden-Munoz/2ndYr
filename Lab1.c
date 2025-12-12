f146f7e746335a0082245710df388b81f35dcf23
dendi cay <munoz.raiden@ue.edu.ph>
2025-12-12 01:18:55 +0000

    lab1 oslab

---
+++ Lab1.c
@@ -0,0 +1,28 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include  <unistd.h>
+#include <sys/wait.h>
+
+void main(int argc, char *arg[])
+{
+	int pid; 
+    pid = fork();
+		if (pid < 0)
+		{
+			printf("fork failed");
+			exit (1);
+		}
+		else if (pid == 0)
+		{
+      		execlp("whoami", "1s", NULL);
+      		exit(0);
+	    }
+	    else
+	    {
+		    printf("\n Process ID is:   %d \n", getpid());
+		    wait (NULL);
+		    exit (0);
+	    }
+        //Raiden Munoz 2CPE - 2B
+}
+
